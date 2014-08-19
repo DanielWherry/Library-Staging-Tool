3825007e7e3d305948d5b619c55bc18315b2296f
DanielWherry <wherrydaniel@gmail.com>
2014-08-19 11:25:09 -0400

    Works on 100 nodes, now generalizing node ability

--- staging.c
+++ staging.c
@@ -2,41 +2,128 @@
 #include <stdlib.h>
 #include <string.h>
 #include <mpi.h>
+#include <errno.h>
+#include <getopt.h>
 
-int main(int argc, char** argv){
+typedef enum {
+	root,
+	notRoot
+	} rootOrNot;
+	
+void setSubCommArray(int* subCommArray, int sizeOfSubComm, int rank); 	
 
-	int rank, numProc, i = 0;
+int main(int argc, char** argv, char** envp){
 
+	int rank, numProc, i, sizeOfSubComm = 10;
+	int numNodes = atoi(getenv("PBS_NUM_NODES"));	
+	
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &numProc);
+	MPI_Group worldGroup, subGroup;
+	MPI_Comm subComm;
 
-	int* writeToRamDisk = (int*) malloc(4 * 64);
+ struct option long_options[] = {
+                {"filename", required_argument, 0, 'f' },
+                {NULL,0,0,0}
+        };
+
+        int long_index = 0;
+        int opt = 0;
+	char filename[50];
+
+
+        while(( opt = getopt_long(argc, argv,"f:",long_options, &long_index)) != -1){
+                switch(opt){
+                        case 'f' :
+				strcpy(filename, optarg);
+				break;
+		}
+	}	
 	
-	for( i = 0; i < 64; i++){
-		writeToRamDisk[i] = i;
+	rootOrNot isRoot = notRoot;
+
+	char* buf;
+	int* subCommArray = (int*) malloc(sizeof(int) * sizeOfSubComm);
+	
+	setSubCommArray(subCommArray, sizeOfSubComm, rank); 	
+
+	MPI_Comm_group(MPI_COMM_WORLD, &worldGroup);
+	MPI_Group_incl(worldGroup, sizeOfSubComm, subCommArray, &subGroup);
+	MPI_Comm_create(MPI_COMM_WORLD, subGroup, &subComm);
+	if(subComm == MPI_COMM_NULL){
+		printf("Ya done messed up\n");
+		return 0;
 	}
 
-	if(rank == 0 || rank == 16 || rank == 32 || rank == 48){
+	if(rank % 10 == 0){
+		isRoot = root;
+	}
+	
+
+	int libraryFileSize;
+	if(isRoot == root){
+		FILE* libraryFile = fopen(filename, "r");
+		if(libraryFile == NULL){
+			printf("Library file pointer is NULL, see errno: %d\n", errno);
+		}
 		
-		FILE* ramDisk;	
-		printf("Hello\n");
-		fopen("/tmp/scratch", "wb");
-		printf("nope\n");
+		fseek(libraryFile, 0, SEEK_END);
+		libraryFileSize = ftell(libraryFile);
+		rewind(libraryFile);
+	
+		buf = (char*) malloc(libraryFileSize);
+		
+		for(i = 0; i < libraryFileSize; i++){
+			buf[i] = i;
+		}
 
-		fwrite(writeToRamDisk, 4, 63, ramDisk);
-		printf("ya\n");
+		fread(buf, sizeof(char), libraryFileSize, libraryFile);
 
-		fclose(ramDisk);
-		ramDisk = NULL; 
-		printf("gotcha\n");
+		fclose(libraryFile);
 	}
 
-	free(writeToRamDisk);
+	MPI_Bcast(&libraryFileSize, 1, MPI_INT, 0, subComm);
+
+	if(isRoot == notRoot){
+		buf = (char*) malloc(libraryFileSize);
+	}
+	
+	MPI_Bcast(buf, libraryFileSize, MPI_CHAR, 0, subComm);
+
+
+	char ramDiskDirectory[100] = "/tmp/scratch/";
+	FILE* ramDisk = fopen(strcat(ramDiskDirectory,filename), "w");
+	if(ramDisk == NULL){
+		printf("RAMDISK file pointer is NULL, see errno: %d\n", errno);
+	}
+	
+	fwrite(buf, sizeof(char), libraryFileSize, ramDisk);
+	
+	fclose(ramDisk);
+	ramDisk = NULL; 
+
+	free(buf);
+	free(subCommArray);
 	MPI_Finalize();
 	
 	return 0;
 
 
 }
+
+void setSubCommArray(int* subCommArray, int sizeOfSubComm, int rank){
+	int i;
+	int groupID = rank / sizeOfSubComm;
+	int rootOfGroup = sizeOfSubComm * groupID;
+	for(i = 0; i < sizeOfSubComm; i++){
+		subCommArray[i] = rootOfGroup + i;
+	}
+}
+
+
+
+
+
+
 

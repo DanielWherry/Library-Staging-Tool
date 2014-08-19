66419902b03fc16a414033f2c698553ef83c8c13
DanielWherry <wherrydaniel@gmail.com>
2014-08-19 15:24:54 -0400

    Working on any number of nodes

--- staging.c
+++ staging.c
@@ -10,18 +10,24 @@ typedef enum {
 	notRoot
 	} RootOrNot;
 typedef struct {
+	char* libFile;
 	int* subCommArray;
-	int sizeOfSubComm;
-	char* buf;
+	int numNodesInNormalComm;
+	int numNormalComms;
+	int numNodesInSmallComm;
 	int libraryFileSize;
+	int sizeOfSubComm;
 	} FileAndMPI;
 	
-void setSubCommArray(FileAndMPI* info, int rank); 	
+void setSubCommArray(FileAndMPI* info, int numNodes, int rank); 	
 
 int main(int argc, char** argv, char** envp){
 
 	int rank, numProc, i;
 	int numNodes = atoi(getenv("PBS_NUM_NODES"));	
+	char filename[50];
+	RootOrNot isRoot = notRoot;
+	FileAndMPI info;
 	
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -29,39 +35,25 @@ int main(int argc, char** argv, char** envp){
 	MPI_Group worldGroup, subGroup;
 	MPI_Comm subComm;
 
- struct option long_options[] = {
-                {"filename", required_argument, 0, 'f' },
-                {NULL,0,0,0}
-        };
-
-        int long_index = 0;
-        int opt = 0;
-	char filename[50];
-
-
-        while(( opt = getopt_long(argc, argv,"f:",long_options, &long_index)) != -1){
-                switch(opt){
-                        case 'f' :
+	struct option long_options[] = {
+		{"filename", required_argument, 0, 'f' },
+        	{NULL,0,0,0}
+       	};
+	int long_index = 0;
+	int opt = 0;
+	
+	while(( opt = getopt_long(argc, argv,"f:",long_options, &long_index)) != -1){
+		switch(opt){
+			case 'f' :
 				strcpy(filename, optarg);
 				break;
 		}
-	}	
-	
-	RootOrNot isRoot = notRoot;
-	FileAndMPI info;
-
-	info.sizeOfSubComm = 10;
-	info.subCommArray = (int*) malloc(sizeof(int) * info.sizeOfSubComm);
-		
-	setSubCommArray(&info, rank); 	
+	}		
+	setSubCommArray(&info, numNodes, rank); 	
 
 	MPI_Comm_group(MPI_COMM_WORLD, &worldGroup);
 	MPI_Group_incl(worldGroup, info.sizeOfSubComm, info.subCommArray, &subGroup);
 	MPI_Comm_create(MPI_COMM_WORLD, subGroup, &subComm);
-	if(subComm == MPI_COMM_NULL){
-		printf("Ya done messed up\n");
-		return 0;
-	}
 
 	if(rank % 10 == 0){
 		isRoot = root;
@@ -77,24 +69,21 @@ int main(int argc, char** argv, char** envp){
 		info.libraryFileSize = ftell(libraryFile);
 		rewind(libraryFile);
 	
-		info.buf = (char*) malloc(info.libraryFileSize);
+		info.libFile = (char*) malloc(info.libraryFileSize);
 		
 		for(i = 0; i < info.libraryFileSize; i++){
-			info.buf[i] = i;
+			info.libFile[i] = i;
 		}
 
-		fread(info.buf, sizeof(char), info.libraryFileSize, libraryFile);
-
+		fread(info.libFile, sizeof(char), info.libraryFileSize, libraryFile);
 		fclose(libraryFile);
 	}
 
 	MPI_Bcast(&(info.libraryFileSize), 1, MPI_INT, 0, subComm);
-
 	if(isRoot == notRoot){
-		info.buf = (char*) malloc(info.libraryFileSize);
+		info.libFile = (char*) malloc(info.libraryFileSize);
 	}
-	
-	MPI_Bcast(info.buf, info.libraryFileSize, MPI_CHAR, 0, subComm);
+	MPI_Bcast(info.libFile, info.libraryFileSize, MPI_CHAR, 0, subComm);
 
 
 	char ramDiskDirectory[100] = "/tmp/scratch/";
@@ -103,32 +92,39 @@ int main(int argc, char** argv, char** envp){
 		printf("RAMDISK file pointer is NULL, see errno: %d\n", errno);
 	}
 	
-	fwrite(info.buf, sizeof(char), info.libraryFileSize, ramDisk);
-	
+	fwrite(info.libFile, sizeof(char), info.libraryFileSize, ramDisk);
 	fclose(ramDisk);
-	ramDisk = NULL; 
 
-	free(info.buf);
+	free(info.libFile);
 	free(info.subCommArray);
 	MPI_Finalize();
 	
 	return 0;
-
-
 }
 
-void setSubCommArray(FileAndMPI* info, int rank){
+void setSubCommArray(FileAndMPI* info, int numNodes, int rank){
+	info->numNodesInNormalComm = 10;
+	info->numNormalComms = numNodes / info->numNodesInNormalComm;
+	info->numNodesInSmallComm = numNodes - (info->numNodesInNormalComm * info->numNormalComms);	
 	int i;
-	int groupID = rank / info->sizeOfSubComm;
-	int rootOfGroup = info->sizeOfSubComm * groupID;
-	for(i = 0; i < info->sizeOfSubComm; i++){
-		info->subCommArray[i] = rootOfGroup + i;
+	int groupID = rank / info->numNodesInNormalComm;
+	int rootOfGroup = info->numNodesInNormalComm * groupID;
+
+	if(groupID < info->numNormalComms){
+		info->subCommArray = (int*) malloc(sizeof(int) * info->numNodesInNormalComm);
+
+		for(i = 0; i < info->numNodesInNormalComm; i++){
+			info->subCommArray[i] = rootOfGroup + i;
+		}
+		info->sizeOfSubComm = info->numNodesInNormalComm;
+	}else{
+		info->subCommArray = (int*) malloc(sizeof(int) * info->numNodesInSmallComm);
+		
+		for(i = 0; i < info->numNodesInSmallComm; i++){
+			info->subCommArray[i] = rootOfGroup + i;
+		}
+		info->sizeOfSubComm = info->numNodesInSmallComm;
 	}
 }
 
-
-
-
-
-
-
+	
